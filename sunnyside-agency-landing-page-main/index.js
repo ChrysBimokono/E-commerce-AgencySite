9b5ef97a034927471aefe418f83b4ab804e7e370
Chrys <cbimokono@gmail.com>
2022-01-07 20:32:36 -0500

    finished with javacript for navigation

--- sunnyside-agency-landing-page-main/index.js
+++ sunnyside-agency-landing-page-main/index.js
@@ -1,26 +1,21 @@
-var nav= document.getElementById('primary-navigation');
-var buttonToggle= document.getElementsByClassName('mobile-nav-toggle')[0];
-var headerT=document.getElementsByClassName('header-text')[0];
-var arrow= document.getElementsByClassName('arrow')[0];
-console.log(headerT)
-console.log(buttonToggle);
-buttonToggle.addEventListener('click',()=>{
+var menuButton= document.getElementsByClassName('mobile-nav-toggle')[0];
+var PrimaryNav= document.getElementsByClassName('primary-navigation')[0]
+console.log(menuButton)
+menuButton.addEventListener('click',function(){
     console.log('clicked')
-    var visibility= nav.getAttribute('data-visible');
+    var visibility=  PrimaryNav.getAttribute('data-visible');
     console.log(visibility)
     if(visibility === 'false'){
-        nav.setAttribute('data-visible', true);
-        nav.setAttribute('aria-expanded',true);
-        nav.style.transform= 'none';
-        headerT.style.display='none';
-        arrow.style.display='none'
-    } else {
-        nav.setAttribute('data-visible', false);
-        nav.setAttribute('aria-expanded',false);
-        nav.style.transform= 'translatex(300%) rotate(360deg) scale(2)';
-        nav.style.transition= '2s ease-in-out';
-        arrow.style.transition= '2s ease-in-out'
-        headerT.style.display='block'
-        arrow.style.display='block'
+        PrimaryNav.setAttribute("data-visible",true);
+        PrimaryNav.setAttribute("aria-expanded", true)
+         PrimaryNav.style.transform='none'
+    } else{
+        PrimaryNav.setAttribute("data-visible",false);
+        PrimaryNav.setAttribute("aria-expanded", false)
+         PrimaryNav.style.transform='translateX(300%) rotate(360deg) scale(2)'
+         PrimaryNav.style.transition= '2s ease-in-out'
     }
+
+    console.log(PrimaryNav.getAttribute('aria-expanded'))
+    
 })
